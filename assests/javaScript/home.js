b6947af664571181b13bb3e23d38f3e234b55b7a
Shamim <mim24sd@gmail.com>
2022-10-03 22:05:24 +0330

    show error message in home

--- assests/javaScript/home.js
+++ assests/javaScript/home.js
@@ -6,7 +6,7 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => showMovies(data.results))
-  .catch((error) => alert(`error: ${error}`));
+  .catch((error) => showErrorMessage());
 
 function showMovies(movies) {
   let markUp = "";
@@ -27,7 +27,7 @@ function showMovies(movies) {
         </div>
       </a>
 </li>`;
-console.log(movie);
+    console.log(movie);
 
     movieList.innerHTML += markUp;
   });
@@ -41,8 +41,14 @@ function queryParam(movie) {
     .join("&");
 }
 
-// file:///home/shamim/code/movie-box/movieDetail.html?adult=false&backdrop_path=%2FrSPw7tgCH9c6NqICZef4kZjFOQ5.jpg&genre_ids=18%2C80&id=238&original_language=en&original_title=The%20Godfather&overview=Spanning%20the%20years%201945%20to%201955%2C%20a%20chronicle%20of%20the%20fictional%20Italian-American%20Corleone%20crime%20family.%20When%20organized%20crime%20family%20patriarch%2C%20Vito%20Corleone%20barely%20survives%20an%20attempt%20on%20his%20life%2C%20his%20youngest%20son%2C%20Michael%20steps%20in%20to%20take%20care%20of%20the%20would-be%20killers%2C%20launching%20a%20campaign%20of%20bloody%20revenge.&popularity=95.057&poster_path=%2F3bhkrj58Vtu7enYsRolD1fZdja1.jpg&release_date=1972-03-14&title=The%20Godfather&video=false&vote_average=8.7&vote_count=16668
+function showErrorMessage() {
+  let markUp = "";
 
+  markUp = `<p class="error-message">Somthing went wrong.Please,refresh the page.</p>`;
+  movieList.innerHTML = markUp;
+}
+
+// file:///home/shamim/code/movie-box/movieDetail.html?adult=false&backdrop_path=%2FrSPw7tgCH9c6NqICZef4kZjFOQ5.jpg&genre_ids=18%2C80&id=238&original_language=en&original_title=The%20Godfather&overview=Spanning%20the%20years%201945%20to%201955%2C%20a%20chronicle%20of%20the%20fictional%20Italian-American%20Corleone%20crime%20family.%20When%20organized%20crime%20family%20patriarch%2C%20Vito%20Corleone%20barely%20survives%20an%20attempt%20on%20his%20life%2C%20his%20youngest%20son%2C%20Michael%20steps%20in%20to%20take%20care%20of%20the%20would-be%20killers%2C%20launching%20a%20campaign%20of%20bloody%20revenge.&popularity=95.057&poster_path=%2F3bhkrj58Vtu7enYsRolD1fZdja1.jpg&release_date=1972-03-14&title=The%20Godfather&video=false&vote_average=8.7&vote_count=16668
 
 // {
 //   adult: false,
