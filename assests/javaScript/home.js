5fcbbdb1db7881098b5b1e34bc9a19754c767eb0
Shamim <mim24sd@gmail.com>
2022-10-03 10:51:47 +0330

    comment movie object

--- assests/javaScript/home.js
+++ assests/javaScript/home.js
@@ -27,6 +27,7 @@ function showMovies(movies) {
         </div>
       </a>
 </li>`;
+console.log(movie);
 
     movieList.innerHTML += markUp;
   });
@@ -41,3 +42,21 @@ function queryParam(movie) {
 }
 
 // file:///home/shamim/code/movie-box/movieDetail.html?adult=false&backdrop_path=%2FrSPw7tgCH9c6NqICZef4kZjFOQ5.jpg&genre_ids=18%2C80&id=238&original_language=en&original_title=The%20Godfather&overview=Spanning%20the%20years%201945%20to%201955%2C%20a%20chronicle%20of%20the%20fictional%20Italian-American%20Corleone%20crime%20family.%20When%20organized%20crime%20family%20patriarch%2C%20Vito%20Corleone%20barely%20survives%20an%20attempt%20on%20his%20life%2C%20his%20youngest%20son%2C%20Michael%20steps%20in%20to%20take%20care%20of%20the%20would-be%20killers%2C%20launching%20a%20campaign%20of%20bloody%20revenge.&popularity=95.057&poster_path=%2F3bhkrj58Vtu7enYsRolD1fZdja1.jpg&release_date=1972-03-14&title=The%20Godfather&video=false&vote_average=8.7&vote_count=16668
+
+
+// {
+//   adult: false,
+//   backdrop_path: "/rSPw7tgCH9c6NqICZef4kZjFOQ5.jpg",
+//   genre_ids: [18, 80],
+//   id: 238,
+//   original_language: "en",
+//   original_title: "The Godfather",
+//   overview: "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.",
+//   popularity: 95.057,
+//   poster_path: "/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+//   release_date: "1972-03-14",
+//   title: "The Godfather",
+//   video: false,
+//   vote_average: 8.7,
+//   vote_count: 16668
+// }
